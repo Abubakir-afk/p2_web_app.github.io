655261c45ae3be6875647788030d4ee4adb8eafd
Abubakr <abubakirzoki@gmail.com>
2025-05-10 10:17:58 +0300

    ozgarish

--- commands.py
+++ commands.py
@@ -8,4 +8,4 @@ router = Router()
 @router.message(CommandStart())
 async def command_start(message: types.Message):
     await message.answer("Keling, boshlaymiz 🍟\n"
-            "Ajoyib tushlikka buyurtma berish uchun quyidagi tugmani bosing!", reply_markup=web_app_btn)
+                            "Ajoyib tushlikka buyurtma berish uchun quyidagi tugmani bosing!", reply_markup=web_app_btn)
