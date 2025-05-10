8d262dfb46356b93a6384b494b23688276f2f758
Abubakr <abubakirzoki@gmail.com>
2025-05-10 10:14:39 +0300

    maxsulotlar qoshilishi

--- keyboards.py
+++ keyboards.py
@@ -2,5 +2,5 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 web_app = WebAppInfo(url="https://abubakir-afk.github.io/p2_web_app.github.io/")
 web_app_btn = InlineKeyboardMarkup(inline_keyboard=[
-    [InlineKeyboardButton(text="Web App", web_app=web_app)]
+    [InlineKeyboardButton(text="Buyurtma qiling", web_app=web_app)]
 ])
